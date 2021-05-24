deff8ff2c556c6c09135ffaa6bbedba7df5c5fd5
Ben Cookson <bendcookson@gmail.com>
2021-05-24 16:00:51 -0300

    Update ReadMe

--- condition_check.py
+++ condition_check.py
@@ -1,4 +1,5 @@
 from profile_tester import generate_all_profiles, all_profiles_results
+import itertools
 
 def verify_pareto_efficient(voting_system, alternatives, voter_count):
     profiles = generate_all_profiles(voter_count,alternatives)
@@ -15,3 +16,8 @@ def verify_pareto_efficient(voting_system, alternatives, voter_count):
                 if prefered and results[i].is_weakly_prefered(other_alt, alt):
                     return False
     return True
+
+def verify_independance_of_irrelevant_alternatives(voting_system, alternatives, voter_count):
+    profiles = generate_all_profiles(voter_count,alternatives)
+    results = all_profiles_results(voter_count,alternatives,voting_system)
+    alt_pairs = list(itertools.combinations(alternatives,2))
